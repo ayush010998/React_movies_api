8d68ca93a31ed328199a3a1b59ea5db09f1723d1
Ayush Anand <ayushanand428@gmail.com>
2023-09-02 11:50:41 -0700

    movieDB

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import './App.css';
+import {React,useState,useEffect} from 'react';
+import MovieBox from './MovieBox';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar,Container,Nav,Form,FormControl,Button } from 'react-bootstrap';
+
+function App() {
+
+  const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=4670c221fa54fe5122ab391ae4801464";
+  const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=4670c221fa54fe5122ab391ae4801464&query"
+
+
+  const [movies,setMovies]=useState([]);
+  const [query,setQuery]=useState('');
+
+  useEffect(()=>{
+    fetch(API_URL)
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data)
+      setMovies(data.results)
+    })
+
+  },[])
+
+  const searchMovie=async(e)=>{
+    e.preventDefault();
+    console.log("Searching...")
+    try{
+      const url=`https://api.themoviedb.org/3/search/movie?api_key=4670c221fa54fe5122ab391ae4801464&query=${query}`
+      const res=await fetch(url);
+      const data=await res.json();
+      console.log(data);
+      setMovies(data.results);
+
+
+
+    }catch(e){
+      console.log(e);
+    }
+
+  }
+  
+  const changeHandler=(e)=>{
+    setQuery(e.target.value)
+  }
+
+  return (
+    <>
+    <Navbar bg='dark' expand='lg' variant='dark'>
+      <Container fluid>
+        <Navbar.Brand href='/home'><b>GSynergy</b> Movie Application</Navbar.Brand>
+        <Navbar.Brand href='/home'>Trending</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="navbarScroll" >
+        </Navbar.Toggle>
+
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav className="me-auto my-2 my-lg-3 " style={{maxHeight:'100px'}} navbarScroll></Nav>
+
+            <Form className="d-flex " onSubmit={searchMovie}>
+              <FormControl
+              type="search"
+              placeholder="Movie Search"
+              className="me-2"
+              aria-label="search"
+              name="query" value={query} onChange={changeHandler}/>
+              <Button variant="secondary" type="submit">Search</Button>
+
+            </Form>
+          </Navbar.Collapse>
+       
+
+      </Container>
+
+    </Navbar>
+    <div>
+      {movies.length>0 ? (<div className='container'>
+      <div className='grid'>
+      {movies.map((movieReq)=>
+      <MovieBox key={movieReq.id} {...movieReq}/>)}
+       </div>  
+       </div>   
+      ):(
+        <h2>Sorry no movies with that name:(</h2>
+      )}
+      
+      </div>
+   
+     
+
+ 
+    </>
+    
+  );
+}
+
+export default App;
